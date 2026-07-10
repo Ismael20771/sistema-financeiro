eaccfc15cd969dfc277e373e689470334967cd49
Ismael20771 <ismael.lima12052001@gmail.com>
2026-07-10 13:21:03 -0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,34 +2,21 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
+despesas = []
 
 @app.get("/")
 def inicio():
     return{"mensagem": "conectado"}
 
+class Despesa(BaseModel):
+    nome:str
+    valor:float
+    descricao:str
 @app.post("/despesas")
-def cria_despesas(nome: str, descricao:str, valor:float):
-    nova_despesa = {
-        "nome": nome,
-        "descricao": descricao,
-        "valor": valor
-    }
+async def criar_despesa(despesa: Despesa):
+    despesas.append(despesa)
+    return {"mensagem":"Despesa cadastrada"}
 
 @app.get("/despesas")
 def lista_despesas():
-    return [
-         {"nome":"parcela", "descricao": "casa", "valor": 1000.00},
-        {"nome":"parcela","descricao": "cabelo", "valor": 30.00}
-    ]
-
-@app.post("/receitas")
-class receitas(BaseModel):
-    descricao:str
-    valor : float
-@app.get("/receitas")
-def lista_recitas():
-    return   [
-        {"descricao": "salario", "valor": 2000.00},
-        {"descricao": "freelancer", "valor": 150.00}
-
-    ]
+    return despesas
